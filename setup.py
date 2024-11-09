dc53aa27ec9e9b0bc78e4f7c0a0c846ca60f9251
Amit Kumar <amitkumar990374@gmail.com>
2024-11-09 13:56:01 +0530

    My first Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Scrapper',
+    version='0.0.1',
+    author='Amit',
+    author_email='amitkumar990374@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
